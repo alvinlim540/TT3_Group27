caaa7211f74110d7a6a96ad1e9039ee5c9cc154e
alvin <alvin_limjy@hotmail.com>
2021-03-05 14:14:50 +0800

    Post REquest is working .

--- dbshack/src/Component/TransactionHistory.js
+++ dbshack/src/Component/TransactionHistory.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { TransactionTable } from "./TransactionTable";
+import SubmitButton from "./submitButton";
 
 class TransactionHistory extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
+			items: [],
+			isLoaded: false,
+
 			transactionId: "",
 			orderType: "",
 			timestamp: "",
@@ -18,40 +22,56 @@ class TransactionHistory extends Component {
 		};
 	}
 
-	changeHandler = (e) => {
-		//for example. this is event
-		// key     :       value
-		this.setState({ [e.target.name]: e.target.value });
-	};
+	componentDidMount() {
+		
+	}
+
+	async getTransactionHistory() {
+		var data = JSON.stringify({
+			accountKey: "8ee1f2c6-ef52-4a6e-ae4c-1dbc5b6f0924",
+		});
+		var config = {
+			method: "post",
+			mode: "no-cors", // cross origin resource sharing
+			url:
+				"https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				"x-api-key": "vSxwt0bA4J2JMaIfDOrAZ7YUykHi7v64lBhi1Eug",
+			},
+			data: data,
+		};
+
+		axios(config)
+			.then((response) => {
+				console.log(response.data);
+				return response.data;
+				
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		
+	}
 
 	submitHandler = (e) => {
 		e.preventDefault();
 		console.log(this.state);
 		// Need to do a post request.
-		axios({
-			method: "post",
-			url:
-				"https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view",
-			headers: {
-				"x-api-key": "vSxwt0bA4J2JMaIfDOrAZ7YUykHi7v64lBhi1Eug",
-			},
-			data: {
-				firstName: "Fred",
-				lastName: "Flintstone",
-			},
-		})
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
 	};
 
 	render() {
 		return (
+			var { isLoaded, items } = this.state;
+
 			<div>
 				TransactionHistory
+				<SubmitButton
+					text="Click me for transaction history"
+					disabled={this.state.buttonDisabled}
+					onClick={() => this.getTransactionHistory()}
+				/>
 				<TransactionTable></TransactionTable>
 			</div>
 		);
