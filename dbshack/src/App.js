02ec4751cdfd3cf60e9f333899f1e8f5098ad62b
Nicholas Loh <51433024+0nicholasloh@users.noreply.github.com>
2021-03-05 12:33:12 +0800

    Login Page
    No function, just the display

--- dbshack/src/App.js
+++ dbshack/src/App.js
@@ -1,9 +1,29 @@
+import React, {useState} from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../src/components/Dashboard/Dashboard';
+import Login from '../src/components/Login/Login';
+import Preferences from '../src/components/Preferences/Preferences';
 
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      hello
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
